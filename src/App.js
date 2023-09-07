0c0b741435fb3e58964ef979a478305a2b358a66
Taukirahmed Khatri <khatritaukirahmed@gmail.com>
2023-09-07 20:49:42 +0530

    Added initial packages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
-	return <div>App component</div>;
+	return <div>This is an entry component of our contacts app!</div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
