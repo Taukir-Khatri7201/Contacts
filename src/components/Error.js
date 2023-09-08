b2b55d8065d96407bb10c4ab4cc1ec18b743a66f
Taukirahmed Khatri <khatritaukirahmed@gmail.com>
2023-09-09 00:45:52 +0530

    Enhanced Error Component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-	return <h2>Oops! The resource you are looking for is not found!</h2>;
+	const error = useRouteError();
+	console.log(error);
+
+	return (
+		<div id="error-page">
+			<h1>Oops!</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+			<p>
+				<i>{error.statusText || error.message}</i>
+			</p>
+		</div>
+	);
 };
 
 export default Error;
