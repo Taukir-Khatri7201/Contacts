1cfa8284f4d643cc3da28a8d3ee7bb044e03e23d
Taukirahmed Khatri <khatritaukirahmed@gmail.com>
2023-09-09 00:20:01 +0530

    Added Error Component

---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+	return <h2>Oops! The resource you are looking for is not found!</h2>;
+};
+
+export default Error;
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,9 +1,13 @@
 import Root from "../components/Root";
+import Error from "../components/Error";
 
+// We need to provide the Error component to errorElement prop for the path to fallback to
+// if the path does not match with any route or subroutes.
 export default routes = [
 	{
 		path: "/",
 		element: <Root />,
 		children: [],
+		errorElement: <Error />,
 	},
 ];
